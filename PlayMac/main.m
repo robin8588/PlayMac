2edbba52fa97b7f293b299f09d1ad393d3c1402b
craig8588 <Craig8588@sina.com>
2013-06-06 17:08:08 +0800

    Start

---
+++ PlayMac/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PlayMac
+//
+//  Created by Wang Leo on 13-6-6.
+//  Copyright (c) 2013年 Wang Leo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

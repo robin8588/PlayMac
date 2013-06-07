de9eb7279ee8868e537efb576e677618544cf40d
craig8588 <Craig8588@sina.com>
2013-06-07 15:17:59 +0800

    Time Zone

--- PlayMac/main.m
+++ PlayMac/main.m
@@ -12,21 +12,11 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        NSDateComponents *comps=[[NSDateComponents alloc] init];
-        [comps setYear:1985];
-        [comps setMonth:8];
-        [comps setDay:8];
-        [comps setHour:5];
-        [comps setMinute:0];
-        [comps setSecond:0];
+        NSTimeZone  *zone=[NSTimeZone systemTimeZone];
+        NSString *name=[zone name];
         
-        NSCalendar *g =[[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
-        NSDate *dateOfBirth=[g dateFromComponents:comps];
+        NSLog(@"you are in %@ timezone",name);
         
-        NSDate *now=[NSDate date];
-        double d=[now timeIntervalSinceDate:dateOfBirth];
-        
-        NSLog(@"this is how i alive: %f seconds or %f minute  or %f hours or %f days or %f years",d,d/60,d/60/60,d/60/60/24,d/60/60/24/365);
     }
     return 0;
 }
